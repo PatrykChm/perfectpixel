e512a77539356d1f322b95c582a9c1a5aa3f4833
Patryk <patrykchmielewskipl@gmail.com>
2024-11-24 17:28:14 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+"use strict";
+var navList = document.querySelector(".navbar__list"),
+    menuButton = document.querySelector(".hamburger"),
+    showMobileMenu = function() {
+        navList.classList.toggle("show-menu")
+    },
+    removeMobileMenu = function() {
+        navList.classList.remove("show-menu")
+    };
+menuButton.addEventListener("click", showMobileMenu), navList.addEventListener("click", removeMobileMenu);
