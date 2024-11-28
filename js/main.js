45c5a19def1afd284c7d92bbbe6a515cbb386ae2
Patryk <patrykchmielewskipl@gmail.com>
2024-11-28 21:58:19 +0100

    add mobile ver of header, add slider header

--- js/main.js
+++ js/main.js
@@ -9,3 +9,17 @@ var navList = document.querySelector('.navbar__list'),
 	}
 menuButton.addEventListener('click', showMobileMenu), navList.addEventListener('click', removeMobileMenu)
 
+var swiper = new Swiper('.mySwiper', {
+	// autoplay: {
+    //     delay: 5500,
+    //     disableOnInteraction: false
+    //   },
+	pagination: {
+		el: '.swiper-pagination',
+		type: 'progressbar',
+	},
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+})
