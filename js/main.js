810614b267a3d502ba3f951d5a15e0812a92433a
Patryk <patrykchmielewskipl@gmail.com>
2024-12-15 00:04:20 +0100

    change menu style and add footer content

--- js/main.js
+++ js/main.js
@@ -1,13 +1,39 @@
-'use strict'
 var navList = document.querySelector('.navbar__list'),
-	menuButton = document.querySelector('.hamburger'),
-	showMobileMenu = function () {
-		navList.classList.toggle('show-menu')
-	},
-	removeMobileMenu = function () {
-		navList.classList.remove('show-menu')
-	}
-menuButton.addEventListener('click', showMobileMenu), navList.addEventListener('click', removeMobileMenu)
+    menuButton = document.querySelector('.hamburger'),
+    navItems = document.querySelectorAll('.navbar__item a'), // Pobieramy linki w menu
+    lastScrollTop = 0, // Przechowujemy poprzednią pozycję scrolla
+    showMobileMenu = function () {
+        navList.classList.toggle('show-menu');
+    },
+    removeMobileMenu = function () {
+        navList.classList.remove('show-menu');
+    },
+    hideMenuOnScroll = function () {
+        // Aktualna pozycja scrolla
+        var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+        // Ukryj menu tylko, jeśli jest widoczne
+        if (navList.classList.contains('show-menu')) {
+            if (Math.abs(currentScroll - lastScrollTop) > 20) {
+                // Ukrywamy menu, jeśli różnica w scrollu jest większa niż 20px
+                removeMobileMenu();
+            }
+        }
+
+        // Zaktualizuj pozycję scrolla
+        lastScrollTop = currentScroll;
+    };
+
+// Pokazanie/ukrycie menu po kliknięciu w hamburgera
+menuButton.addEventListener('click', showMobileMenu);
+
+// Ukrycie menu po kliknięciu w dowolny link w menu
+navItems.forEach(function (item) {
+    item.addEventListener('click', removeMobileMenu);
+});
+
+// Ukrycie menu po scrollowaniu strony
+window.addEventListener('scroll', hideMenuOnScroll);
 
 var swiper = new Swiper('.mySwiper', {
 	// autoplay: {
@@ -30,3 +56,5 @@ document.querySelector('.logos').appendChild(copy)
 console.clear()
 
 AOS.init()
+
+
