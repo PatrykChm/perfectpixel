2021fad31e876649892e91e78e951e5c6a80df0c
Patryk <patrykchmielewskipl@gmail.com>
2025-01-02 20:15:21 +0100

    change some global styles

--- js/main.js
+++ js/main.js
@@ -1,81 +1,80 @@
 var navList = document.querySelector('.navbar__list'),
-    menuButton = document.querySelector('.hamburger'),
-    navItems = document.querySelectorAll('.navbar__item a'), // Pobieramy linki w menu
-    lastScrollTop = 0, // Przechowujemy poprzednią pozycję scrolla
-    showMobileMenu = function () {
-        navList.classList.toggle('show-menu');
-    },
-    removeMobileMenu = function () {
-        navList.classList.remove('show-menu');
-    },
-    hideMenuOnScroll = function () {
-        // Aktualna pozycja scrolla
-        var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+	menuButton = document.querySelector('.hamburger'),
+	navItems = document.querySelectorAll('.navbar__item a'), // Pobieramy linki w menu
+	lastScrollTop = 0, // Przechowujemy poprzednią pozycję scrolla
+	showMobileMenu = function () {
+		navList.classList.toggle('show-menu')
+	},
+	removeMobileMenu = function () {
+		navList.classList.remove('show-menu')
+	},
+	hideMenuOnScroll = function () {
+		// Aktualna pozycja scrolla
+		var currentScroll = window.pageYOffset || document.documentElement.scrollTop
 
-        // Ukryj menu tylko, jeśli jest widoczne
-        if (navList.classList.contains('show-menu')) {
-            if (Math.abs(currentScroll - lastScrollTop) > 20) {
-                // Ukrywamy menu, jeśli różnica w scrollu jest większa niż 20px
-                removeMobileMenu();
-            }
-        }
+		// Ukryj menu tylko, jeśli jest widoczne
+		if (navList.classList.contains('show-menu')) {
+			if (Math.abs(currentScroll - lastScrollTop) > 20) {
+				// Ukrywamy menu, jeśli różnica w scrollu jest większa niż 20px
+				removeMobileMenu()
+			}
+		}
 
-        // Zaktualizuj pozycję scrolla
-        lastScrollTop = currentScroll;
-    };
+		// Zaktualizuj pozycję scrolla
+		lastScrollTop = currentScroll
+	}
 
 // Pokazanie/ukrycie menu po kliknięciu w hamburgera
-menuButton.addEventListener('click', showMobileMenu);
+menuButton.addEventListener('click', showMobileMenu)
 
 // Ukrycie menu po kliknięciu w dowolny link w menu
 navItems.forEach(function (item) {
-    item.addEventListener('click', removeMobileMenu);
-});
+	item.addEventListener('click', removeMobileMenu)
+})
 
 // Ukrycie menu po scrollowaniu strony
-window.addEventListener('scroll', hideMenuOnScroll);
+window.addEventListener('scroll', hideMenuOnScroll)
 
 // Pobranie elementów
-const tabHeaders = document.querySelectorAll('.tab-header');
-const tabPanels = document.querySelectorAll('.tab-panel');
+const tabHeaders = document.querySelectorAll('.tab-header')
+const tabPanels = document.querySelectorAll('.tab-panel')
 
 // Funkcja przełączania
 tabHeaders.forEach(header => {
-    header.addEventListener('click', () => {
-        const tabNumber = header.dataset.tab;
+	header.addEventListener('click', () => {
+		const tabNumber = header.dataset.tab
 
-        // Deaktywacja wszystkich nagłówków i paneli
-        tabHeaders.forEach(h => h.classList.remove('active'));
-        tabPanels.forEach(panel => panel.classList.remove('active'));
+		// Deaktywacja wszystkich nagłówków i paneli
+		tabHeaders.forEach(h => h.classList.remove('active'))
+		tabPanels.forEach(panel => panel.classList.remove('active'))
 
-        // Aktywacja klikniętego nagłówka i odpowiadającego panelu
-        header.classList.add('active');
-        document.querySelector(`.tab-panel[data-tab="${tabNumber}"]`).classList.add('active');
-    });
-});
-
+		// Aktywacja klikniętego nagłówka i odpowiadającego panelu
+		header.classList.add('active')
+		document.querySelector(`.tab-panel[data-tab="${tabNumber}"]`).classList.add('active')
+	})
+})
 
 window.addEventListener('scroll', () => {
-    const navbar = document.querySelector('.navbar__nav');
-    const logo = document.querySelector('.logo img');
-    const scrollThreshold = 50;
+	const navbar = document.querySelector('.navbar__nav')
+	const logo = document.querySelector('.logo img')
+	const scrollThreshold = 50
 
-    if (window.scrollY > scrollThreshold) {
-        // Zmniejsz wysokość menu
-        navbar.style.height = '70px';
-        navbar.style.transition = 'height 0.3s ease';
+	if (window.scrollY > scrollThreshold) {
+		// Zmniejsz wysokość menu
+		navbar.style.height = '70px'
+		navbar.style.transition = 'height 0.3s ease'
 
-        // Zmniejsz rozmiar logo
-        logo.style.maxWidth = '150px'; // Dopasuj do swoich potrzeb
-        logo.style.transition = 'max-width 0.3s ease';
-    } else {
-        // Przywróć domyślną wysokość menu
-        navbar.style.height = '100px';
+		// Zmniejsz rozmiar logo
+		logo.style.maxWidth = '150px' // Dopasuj do swoich potrzeb
+		logo.style.transition = 'max-width 0.3s ease'
+	} else {
+		// Przywróć domyślną wysokość menu
+		navbar.style.height = '100px'
 
-        // Przywróć domyślny rozmiar logo
-        logo.style.maxWidth = '200px';
-    }
-});
+		// Przywróć domyślny rozmiar logo
+		logo.style.maxWidth = '200px'
+	}
+})
 
 
 
@@ -85,5 +84,3 @@ document.querySelector('.logos').appendChild(copy)
 console.clear()
 
 AOS.init()
-
-
